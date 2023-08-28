7b727ff81b3d895a8a6b3c43a6aafbbd2232e217
tommyokoyo <okoyotommy@gmail.com>
2023-08-28 23:14:29 +0300

    LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 """
-    LIFOCache cache class
+    LRU cache class
 """
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """
-        FIFOCache class that inherits from BaseCaching
+        LRUcache class that inherits from BaseCaching
     """
-    last_key_added = ''
+    usage_counter = {}
 
     def __init__(self):
         """
@@ -26,25 +26,21 @@ class LIFOCache(BaseCaching):
             :return: No return value
         """
         if key is None or item is None:
-            pass
+            return
         elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             if key in self.cache_data.keys():
-                if key == self.last_key_added:
-                    del self.cache_data[self.last_key_added]
-                    print("DISCARD: " + self.last_key_added)
-                    self.cache_data[key] = item
-                    self.last_key_added = key
-                else:
-                    self.cache_data[key] = item
-                    self.last_key_added = key
-            else:
-                del self.cache_data[self.last_key_added]
-                print("DISCARD: " + self.last_key_added)
                 self.cache_data[key] = item
-                self.last_key_added = key
+                self.usage_counter[key] = 0
+            else:
+                least_used_key = min(self.usage_counter, key=self.usage_counter.get)
+                del self.cache_data[least_used_key]
+                del self.usage_counter[least_used_key]
+                print("DISCARD: " + least_used_key)
+                self.cache_data[key] = item
+                self.usage_counter[key] = 0
         else:
             self.cache_data[key] = item
-            self.last_key_added = key
+            self.usage_counter[key] = 0
 
     def get(self, key):
         """
@@ -59,4 +55,5 @@ class LIFOCache(BaseCaching):
             if retrieved_data is None:
                 return None
             else:
+                self.usage_counter[key] += 1
                 return retrieved_data
--- 0x01-caching/scratch.py
+++ 0x01-caching/scratch.py
@@ -3,10 +3,8 @@
     my scratch practice file
 """
 
-my_dict = {"a": 1, "b": 2, "c": 3, "d": 4}
+my_dict = {"a": 10, "b": 2, "c": 3, "d": 4}
 
-if my_dict:
-    print("DISCARD: " + str(my_dict.popitem()[0]))
-    print(my_dict)
-else:
-    print('Dictionary empty')
+my_dict['a'] += 1
+print(my_dict)
+
